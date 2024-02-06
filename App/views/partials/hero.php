4b9cd9fca0f3d127d1167d1a7813a9d6ff4aa2c9
yohdev-travis <travis@yohdigital.com>
2024-02-05 20:47:58 -0500

    Hero updates

--- App/views/partials/hero.php
+++ App/views/partials/hero.php
@@ -2,7 +2,8 @@
   <div class="search-container">
     <form action="/products/search" method="POST">
       <div class="form-group d-flex">
-        <input type="text" name="keyword" class="form-control">
+        <input type="text" name="keyword" placeholder="Search..."
+          class="form-control me-3">
         <input type="submit" value="Search" name="search"
           class="btn btn-primary">
       </div>
